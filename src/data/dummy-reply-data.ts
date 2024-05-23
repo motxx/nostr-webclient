849b6082a89d680aab1a06278ef6d1888d32723a
motxx <motohiko.ave@gmail.com>
2024-05-23 20:51:32 +0900

    chore: update mock data

--- src/data/dummy-reply-data.ts
+++ src/data/dummy-reply-data.ts
@@ -1,35 +1,37 @@
-import user1Image from "../assets/images/example/waifu.png";
-import user2Image from "../assets/images/example/man.jpg";
-import { PostType } from "../global/types";
+import user1Image from '../assets/images/example/waifu.png'
+import user2Image from '../assets/images/example/man.jpg'
+import { PostType } from '../global/types'
 
-type PostTypeWithId = PostType & { id: string };
+type PostTypeWithId = PostType & { id: string }
 const replyData: PostTypeWithId[] = [
   {
-    id: "101",
-    userId: "riel.pages.dev",
-    userName: "riel@休職中",
+    id: '101',
+    userId: 'riel.pages.dev',
+    userName: 'riel@休職中',
     verified: false,
-    content: "うおおおおおお！！！",
+    content: 'うおおおおおお！！！',
+    replies: 0,
     likes: 3,
     reposts: 0,
     zaps: 15,
     userImage: user1Image,
-    timestamp: "2 hours ago",
+    timestamp: '2 hours ago',
     following: true,
   },
   {
-    id: "102",
-    userId: "emiliod",
-    userName: "Emilio Drake",
+    id: '102',
+    userId: 'emiliod',
+    userName: 'Emilio Drake',
     verified: true,
-    content: "nice!!!",
+    content: 'nice!!!',
+    replies: 0,
     likes: 0,
     reposts: 0,
     zaps: 0,
     userImage: user2Image,
-    timestamp: "1 hours ago",
+    timestamp: '1 hours ago',
     following: false,
   },
-];
+]
 
-export default replyData;
+export default replyData
