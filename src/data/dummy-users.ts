97af504f5684a5d6d51d26ca55ac138276994a2e
motxx <motohiko.ave@gmail.com>
2024-05-30 22:09:54 +0900

    chore: add dummy-users mock

---
+++ src/data/dummy-users.ts
@@ -0,0 +1,33 @@
+import user1Image from '../assets/images/example/waifu.png'
+import user2Image from '../assets/images/example/man.jpg'
+
+export const users = [
+  {
+    id: '1',
+    userId: 'riel.pages.dev',
+    userName: 'riel@休職中',
+    verified: false,
+    image: user1Image,
+  },
+  {
+    id: '2',
+    userId: 'Ai_Manga',
+    userName: 'AI Manga Artist',
+    verified: true,
+    image: user2Image,
+  },
+  {
+    id: '3',
+    userId: 'emiliod',
+    userName: 'Emilio Drake',
+    verified: true,
+    image: user2Image,
+  },
+  {
+    id: '4',
+    userId: 'usualman',
+    userName: 'Usual Man',
+    verified: false,
+    image: 'https://randomuser.me/api/portraits/men/1.jpg',
+  },
+]
