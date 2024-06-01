2be34a6b643a7984a3fdf419ab5df1dc425841e1
motxx <motohiko.ave@gmail.com>
2024-06-01 17:32:41 +0900

    feat(navigation): ユーザボタンの無駄な演出を減らす

--- src/components/Navigation/NavigationSidebarUserSection.tsx
+++ src/components/Navigation/NavigationSidebarUserSection.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NavigationSidebarUserSection = ({ user }: any) => {
   return (
-    <div className="flex justify-center lg:justify-start items-center lg:space-x-2 p-2 hover:bg-gray-200 dark:hover:bg-gray-700 text-gray-700 dark:text-gray-300 active:bg-gray-100 dark:active:bg-gray-800 rounded-md transition cursor-pointer">
+    <div className="flex justify-center lg:justify-start items-center lg:space-x-2 p-2 hover:bg-gray-200 dark:hover:bg-gray-700 text-gray-700 dark:text-gray-300 rounded-md transition cursor-pointer">
       <img
         src={user.image}
         alt="User profile"
