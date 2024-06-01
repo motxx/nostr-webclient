bbd550aa987031385bbc11e21793a512ba19b75a
motxx <motohiko.ave@gmail.com>
2024-06-01 14:15:14 +0900

    refactor: extract MessageChatSidebar

--- src/pages/MessagePage.tsx
+++ src/pages/MessagePage.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
-import { FiSearch } from 'react-icons/fi'
-import { LuMessageSquarePlus } from 'react-icons/lu'
 import MessageConversation from '../components/Message/MessageConversation'
 import MessageCreateChatModal from '../components/Message/MessageCreateChatModal'
+import MessageChatSidebar from '../components/Message/MessageChatSidebar'
 import { mockConversations } from '../data/dummy-message-conversations'
 import { MessageConversationType } from '../global/types'
 
@@ -42,10 +41,6 @@ const MessagePage: React.FC = () => {
     setConversations(updatedConversations)
   }
 
-  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(event.target.value)
-  }
-
   const handleCreateChat = (chatName: string, participants: string[]) => {
     const newChat = {
       id: String(conversations.length + 1),
@@ -67,57 +62,14 @@ const MessagePage: React.FC = () => {
 
   return (
     <div className="flex h-full">
-      <div className="w-1/3 border-r border-gray-200 dark:border-gray-700">
-        <div className="p-4">
-          <div className="flex items-center mb-4">
-            <h1 className="text-lg font-bold">メッセージ</h1>
-            <button className="ml-auto" onClick={() => setIsModalOpen(true)}>
-              <LuMessageSquarePlus className="text-2xl hover:text-blue-500 transition" />
-            </button>
-          </div>
-          <div className="relative">
-            <div className="flex items-center w-full p-2 border border-gray-200 dark:border-gray-700 rounded-full">
-              <FiSearch className="text-gray-700 dark:text-gray-300 mr-2" />
-              <input
-                type="text"
-                value={searchTerm}
-                onChange={handleSearchChange}
-                placeholder="メッセージを検索"
-                className="bg-transparent outline-none w-full text-sm text-gray-700 dark:text-gray-300"
-              />
-            </div>
-          </div>
-        </div>
-        <ul>
-          {filteredConversations.map((conversation) => (
-            <li
-              key={conversation.id}
-              className="px-4 sm:px-6 py-2 flex items-center hover:bg-gray-100 dark:hover:bg-gray-900 transition duration-300 ease-in-out cursor-pointer"
-              onClick={() => handleSelectConversation(conversation.id)}
-            >
-              <img
-                src={conversation.avatar}
-                alt={conversation.name}
-                className="w-8 h-8 rounded-full mr-2"
-              />
-              <div className="flex flex-col">
-                <span>{conversation.name}</span>
-                {conversation.members.length > 2 && (
-                  <span className="text-xs text-gray-500 dark:text-gray-400">
-                    {conversation.members.join(', ')}
-                  </span>
-                )}
-                <span className="text-xs text-gray-400 dark:text-gray-500 truncate">
-                  {
-                    conversation.messages[conversation.messages.length - 1]
-                      .content
-                  }
-                </span>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <MessageChatSidebar
+        conversations={conversations}
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        filteredConversations={filteredConversations}
+        handleSelectConversation={handleSelectConversation}
+        setIsModalOpen={setIsModalOpen}
+      />
       <div className="flex-1">
         {selectedConversation ? (
           <MessageConversation
