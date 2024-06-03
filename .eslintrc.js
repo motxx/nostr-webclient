d435b07b735f40967aaf2c4e516756429a19486d
motxx <motohiko.ave@gmail.com>
2024-06-04 04:57:40 +0900

    chore: add craco

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['react-app', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: ['@/features/*/*'],
+      },
+    ],
+  },
+}
