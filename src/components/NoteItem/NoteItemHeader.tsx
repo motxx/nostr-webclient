37f4f1dc86bf15f6794eb7e30771172ab8470b7a
motxx <motohiko.ave@gmail.com>
2024-06-15 05:46:15 +0900

    feat: update note item header

--- src/components/NoteItem/NoteItemHeader.tsx
+++ src/components/NoteItem/NoteItemHeader.tsx
@@ -7,6 +7,7 @@ import { NoteType } from '@/domain/entities/Note'
 import { User } from '@/domain/entities/User'
 import { userIdForDisplay, userNameForDisplay } from '@/utils/addressConverter'
 import { formatDateAsString } from '@/utils/timeConverter'
+import Avatar from 'boring-avatars'
 
 interface NoteItemHeaderProps {
   note: NoteType
@@ -23,26 +24,67 @@ const NoteItemHeader: React.FC<NoteItemHeaderProps> = ({
   const closeMenu = () => setShowMenu(false)
 
   const author = {
-    image:
-      note.author.profile?.image ??
-      'https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg',
+    image: note.author.profile?.image,
     name: userNameForDisplay(note.author),
     id: userIdForDisplay(note.author),
     verified: User.verified(note.author),
   }
 
+  /*
+  // 遅延読み込みに対応する場合に使う
+  const observer = useMemo(
+    () =>
+      new IntersectionObserver(async ([entry]) => {
+        if (
+          entry.isIntersecting &&
+          noteItemHeaderRef.current &&
+          nostrClient &&
+          !note.author.profile?.name
+        ) {
+          console.log('fetching profile:', note.author.npub)
+          const userProfileService = new UserProfileService(nostrClient)
+          const profile = await new FetchUserProfile(
+            userProfileService
+          ).execute(note.author.npub)
+          if (profile) {
+            note.author.profile = profile
+          }
+        }
+      }),
+    [note.author, nostrClient]
+  )
+
+  useEffect(() => {
+    const headerRef = noteItemHeaderRef.current
+    if (headerRef) observer.observe(headerRef)
+
+    return () => {
+      if (headerRef) observer.unobserve(headerRef)
+    }
+  }, [observer])
+  */
+
   return (
-    <>
+    <div>
       <div className="flex justify-between items-center font-noto-sans">
         <div
           className="flex items-center space-x-3 cursor-pointer"
           onClick={() => navigate(`/user/${note.author.npub}`)}
         >
-          <img
-            src={author.image}
-            alt={`${author.name}'s profile`}
-            className="w-8 h-8 ml-1 rounded-full"
-          />
+          {author.image ? (
+            <img
+              src={author.image}
+              alt={`${author.name}'s profile`}
+              className="w-8 h-8 ml-1 rounded-full"
+            />
+          ) : (
+            <Avatar
+              size={32}
+              name={note.author.profile?.name || note.author.npub}
+              variant="beam"
+              colors={['#92A1C6', '#146A7C', '#F0AB3D', '#C271B4', '#C20D90']}
+            />
+          )}
           <div>
             <div className="flex items-center">
               <div className="font-semibold text-xs text-gray-700 dark:text-gray-300">
@@ -73,7 +115,7 @@ const NoteItemHeader: React.FC<NoteItemHeaderProps> = ({
           onClose={closeMenu}
         />
       )}
-    </>
+    </div>
   )
 }
 
