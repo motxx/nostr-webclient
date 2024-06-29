942e7328e56cd5970b441391cfae8110e126dbe5
motxx <motohiko.ave@gmail.com>
2024-06-29 17:24:32 +0900

    fix: fix mock

--- src/utils/mock.ts
+++ src/utils/mock.ts
@@ -14,6 +14,7 @@ export const createDummyNewReply = (text: string): NoteType => {
       },
     },
     created_at: new Date(),
+    json: '{}',
     replyChildNotes: [],
     likes: 0,
     reposts: 0,
