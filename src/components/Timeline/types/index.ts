25a16dbc62ec120f233600bdfb8055b42e2a256a
motxx <motohiko.ave@gmail.com>
2024-07-13 11:49:57 +0900

    fix: add missing type def

--- src/components/Timeline/types/index.ts
+++ src/components/Timeline/types/index.ts
@@ -11,3 +11,10 @@ export const HomeTimelineTabs: TimelineTabType[] = [
   { id: 'images', feedType: 'image-grid', name: 'ピクチャー' },
   { id: 'clips', feedType: 'video-swipe', name: 'クリップ' },
 ]
+
+declare global {
+  interface Window {
+    YT: any
+    onYouTubeIframeAPIReady: () => void
+  }
+}
