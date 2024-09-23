db60a375d649743515e0c3b7f7873c3061186372
motxx <motohiko.ave@gmail.com>
2024-09-23 22:18:00 +0900

    feat: improve eventBus

--- src/utils/eventBus.ts
+++ src/utils/eventBus.ts
@@ -1,30 +1,70 @@
-type EventCallback = (...args: any[]) => void
+import { User } from '@/domain/entities/User'
+import { NDKUser } from '@nostr-dev-kit/ndk'
 
-class EventBus {
-  private events: { [key: string]: EventCallback[] } = {}
+type EventMap = {
+  [event: string]: any
+}
 
-  on(event: string, callback: EventCallback) {
+type EventCallback<Payload = any> = (payload: Payload) => void
+
+class EventBus<Events extends EventMap = EventMap> {
+  private events: {
+    [K in keyof Events]?: Set<EventCallback<Events[K]>>
+  } = {}
+
+  on<K extends keyof Events>(event: K, callback: EventCallback<Events[K]>) {
     if (!this.events[event]) {
-      this.events[event] = []
+      this.events[event] = new Set()
     }
-    this.events[event].push(callback)
+    this.events[event]!.add(callback)
   }
 
-  emit(event: string, ...args: any[]) {
+  once<K extends keyof Events>(event: K, callback: EventCallback<Events[K]>) {
+    const onceCallback: EventCallback<Events[K]> = (payload) => {
+      callback(payload)
+      this.off(event, onceCallback)
+    }
+    this.on(event, onceCallback)
+  }
+
+  emit<K extends keyof Events>(event: K, payload: Events[K]) {
     if (this.events[event]) {
-      this.events[event].forEach((callback) => callback(...args))
+      const callbacks = Array.from(this.events[event]!)
+      for (const callback of callbacks) {
+        try {
+          callback(payload)
+        } catch (error) {
+          console.error(
+            `Error in event listener for event "${String(event)}":`,
+            error
+          )
+        }
+      }
     }
   }
 
-  off(event: string, callback: EventCallback) {
+  off<K extends keyof Events>(event: K, callback: EventCallback<Events[K]>) {
     if (this.events[event]) {
-      this.events[event] = this.events[event].filter((cb) => cb !== callback)
+      this.events[event]!.delete(callback)
+      if (this.events[event]!.size === 0) {
+        delete this.events[event]
+      }
     }
   }
 
-  find(event: string) {
-    return this.events[event]
+  removeAllListeners<K extends keyof Events>(event: K) {
+    if (this.events[event]) {
+      delete this.events[event]
+    }
+  }
+
+  hasListeners<K extends keyof Events>(event: K): boolean {
+    return !!(this.events[event] && this.events[event]!.size > 0)
   }
 }
 
-export const eventBus = new EventBus()
+export const eventBus = new EventBus<{
+  mention: { from: string; message: string }
+  dm: { from: string; message: string }
+  login: { user: User }
+}>()
