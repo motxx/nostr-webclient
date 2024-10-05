e3aa35dd52bdd3e58cd317d8d0cb4afb744d9220
motxx <motohiko.ave@gmail.com>
2024-10-05 18:26:14 +0900

    feat: remove redundant file

--- src/components/Timeline/hooks/useNotesTimeline.ts
+++ src/components/Timeline/hooks/useNotesTimeline.ts
@@ -1,7 +1,12 @@
-import { useContext, useEffect, useRef } from 'react'
+import { useCallback, useContext, useEffect, useRef } from 'react'
 import { AppContext } from '@/context/AppContext'
 import { AuthStatus, TimelineStatus } from '@/context/types'
-import { useNotesSubscription } from './useNotesSubscription'
+import { NoteService } from '@/infrastructure/services/NoteService'
+import { SubscribeNotes } from '@/domain/use_cases/SubscribeNotes'
+import { SubscribeNotesOptions } from '@/domain/repositories/NoteRepository'
+import { UserProfileService } from '@/infrastructure/services/UserProfileService'
+import { OperationType } from '@/context/actions'
+import { FetchPastNotes } from '@/domain/use_cases/FetchPastNotes'
 
 interface UseNotesTimelineOptions {
   authorPubkeys?: string[]
@@ -15,16 +20,64 @@ export const useNotesTimeline = ({
   hashtag,
 }: UseNotesTimelineOptions) => {
   const {
-    auth: { status: authStatus },
-    timeline: { status: timelineStatus, notes },
+    auth: { nostrClient, status: authStatus },
+    timeline: { notes, status: timelineStatus, subscription },
+    dispatch,
   } = useContext(AppContext)
-  const { subscribe, unsubscribe } = useNotesSubscription()
 
   const isTimelineLoading =
     timelineStatus !== TimelineStatus.Subscribing || notes.length === 0
 
   // 再レンダリングを防ぐためにuseRefを使う
   const isSubscribing = useRef<boolean>(false)
+
+  const subscribe = useCallback(
+    (options: SubscribeNotesOptions) => {
+      if (
+        (authStatus !== AuthStatus.ClientReady &&
+          authStatus !== AuthStatus.LoggedIn) ||
+        timelineStatus !== TimelineStatus.Idle
+      ) {
+        return
+      }
+      if (!nostrClient) throw new Error('NostrClient is not ready')
+
+      const userProfileService = new UserProfileService(nostrClient)
+      const noteService = new NoteService(nostrClient, userProfileService)
+
+      dispatch({ type: OperationType.FetchPastNotesStart })
+
+      new FetchPastNotes(noteService).execute({ ...options, limit: 20 }).match(
+        (notes) => {
+          dispatch({ type: OperationType.FetchPastNotesEnd, notes })
+        },
+        (error) => {
+          dispatch({ type: OperationType.FetchPastNotesError, error })
+        }
+      )
+
+      const subscription = new SubscribeNotes(noteService)
+        .execute(options)
+        .subscribe({
+          next: (note) => {
+            dispatch({ type: OperationType.AddNewNote, note })
+          },
+          error: (error) => {
+            dispatch({ type: OperationType.SubscribeNotesError, error })
+          },
+        })
+
+      dispatch({ type: OperationType.SubscribeNotes, subscription })
+    },
+    [nostrClient, dispatch, authStatus, timelineStatus]
+  )
+
+  const unsubscribe = useCallback(() => {
+    if (subscription) {
+      subscription.unsubscribe()
+      dispatch({ type: OperationType.UnsubscribeNotes })
+    }
+  }, [subscription, dispatch])
 
   useEffect(() => {
     if (
